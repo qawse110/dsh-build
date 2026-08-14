cc73804243fbaa182d6d73b9fc92f20ae3045dc0
qawse110 <qawse110@users.noreply.github.com>
2026-08-14 05:28:24 +0000

    targetSdk 34: attempt to bypass device exec restriction (version 3.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,9 +9,9 @@ android {
     defaultConfig {
         applicationId = "com.dsh.launcher"
         minSdk = 24
-        targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        targetSdk = 34
+        versionCode = 3
+        versionName = "3.0"
     }
 
     buildTypes {
