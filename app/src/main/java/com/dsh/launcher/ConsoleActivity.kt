032e938e2ec8e6f1b6fbd988da022018b3a67fad
dsh <dsh@local>
2026-08-14 10:57:13 +0000

    fix: add missing Intent import

--- app/src/main/java/com/dsh/launcher/ConsoleActivity.kt
+++ app/src/main/java/com/dsh/launcher/ConsoleActivity.kt
@@ -5,6 +5,7 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
+import android.content.Intent
 import android.os.IBinder
 import android.view.Gravity
 import android.view.View
